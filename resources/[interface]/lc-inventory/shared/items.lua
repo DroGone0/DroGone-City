3bc81b8e5eee62cbd0b5ac47ef6dea7540e77c73
DroGone0 <robin.btl02@gmail.com>
2025-04-29 02:30:39 +0200

    Version base avec ox inventory

---
+++ resources/[interface]/lc-inventory/shared/items.lua
@@ -0,0 +1,11 @@
+local items = {
+    ["water"] = {
+        label = "Bouteille d'eau",
+        weight = 500,         -- 500 grammes
+        usable = true,        -- Peut être utilisé (ex: boire)
+        rare = false,         -- Pas un item rare
+        canRemove = true      -- Peut être retiré de l'inventaire
+    }
+}
+
+return items
---
+++ server-data/start.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\Users\DroGone\OneDrive\Documents\Document_PC1\Projet_Perso\GTA_RP_SERVER\GTA-RP-Server
+C:\Users\DroGone\OneDrive\Documents\Document_PC1\Projet_Perso\GTA_RP_SERVER\GTA-RP-Server\FXServer.exe +exec server-data/server.cfg
+pause
